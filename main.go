c113392fd8dda80e13ab74be3441bab753a7030a
yurka1399 <yurka1399@gmail.com>
2019-08-19 15:20:38 +0300

    small fixes

--- main.go
+++ main.go
@@ -4,29 +4,23 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
-	"hash"
-	"sync"
 )
 
 // HashGenerator Class
 type HashGenerator struct {
-	key  string
-	algo hash.Hash
-	mu   sync.Mutex
+	key string
 }
 
 // Generate generates hash from string passd in toHash
 func (h *HashGenerator) Generate(toHash string) string {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.algo.Write([]byte(fmt.Sprintf("%s%s", toHash, h.key)))
-	return hex.EncodeToString(h.algo.Sum(nil))
+	algo := md5.New()
+	algo.Write([]byte(fmt.Sprintf("%s%s", toHash, h.key)))
+	return hex.EncodeToString(algo.Sum(nil))
 }
 
 // Init returns new HashGenerator object
 func Init(skey string) *HashGenerator {
 	return &HashGenerator{
-		key:  skey,
-		algo: md5.New(),
+		key: skey,
 	}
 }
